4000de27ac14f7fc923f1fb469eadb025f569327
Paulus <you@example.com>
2023-08-22 07:21:52 +0700

    update

--- script.js
+++ script.js
@@ -1,57 +1,6 @@
-const addBtn = document.getElementById("addBtn");
-const taskList = document.getElementById("taskList");
-
-const storedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
-
-storedTasks.forEach((task) => {
-  createTaskItem(task);
-});
-
-addBtn.addEventListener("click", function () {
-  const taskInput = document.getElementById("task");
-  const deadlineInput = document.getElementById("deadline");
-  const priorityInput = document.getElementById("priority");
-
-  const task = taskInput.value;
-  const deadline = deadlineInput.value;
-  const priority = priorityInput.value;
-
-  if (task && deadline && priority) {
-    const newTask = { task, deadline, priority };
-    storedTasks.push(newTask);
-    localStorage.setItem("tasks", JSON.stringify(storedTasks));
-
-    createTaskItem(newTask);
-
-    taskInput.value = "";
-    deadlineInput.value = "";
-    priorityInput.value = "Tinggi";
-  }
-});
-
-function createTaskItem(task) {
-  const taskItem = document.createElement("div");
-  taskItem.classList.add("task-item");
-  taskItem.innerHTML = `
-      <p>Tugas : ${task.task}</p>
-      <p>Deadline : ${task.deadline}</p>
-      <p>Prioritas : ${task.priority}</p>
+const addBtn=document.getElementById("addBtn"),taskList=document.getElementById("taskList"),storedTasks=JSON.parse(localStorage.getItem("tasks"))||[];function createTaskItem(e){let t=document.createElement("div");t.classList.add("task-item"),t.innerHTML=`
+      <p>Tugas : ${e.task}</p>
+      <p>Deadline : ${e.deadline}</p>
+      <p>Prioritas : ${e.priority}</p>
       <button class="delete-btn">Hapus</button>
-    `;
-  taskList.appendChild(taskItem);
-
-  const deleteBtn = taskItem.querySelector(".delete-btn");
-  deleteBtn.addEventListener("click", function () {
-    const taskIndex = storedTasks.findIndex(
-      (item) =>
-        item.task === task.task &&
-        item.deadline === task.deadline &&
-        item.priority === task.priority
-    );
-    if (taskIndex !== -1) {
-      storedTasks.splice(taskIndex, 1);
-      localStorage.setItem("tasks", JSON.stringify(storedTasks));
-    }
-    taskItem.remove();
-  });
-}
+    `,taskList.appendChild(t);let s=t.querySelector(".delete-btn");s.addEventListener("click",function(){let s=storedTasks.findIndex(t=>t.task===e.task&&t.deadline===e.deadline&&t.priority===e.priority);-1!==s&&(storedTasks.splice(s,1),localStorage.setItem("tasks",JSON.stringify(storedTasks))),t.remove()})}storedTasks.forEach(e=>{createTaskItem(e)}),addBtn.addEventListener("click",function(){let e=document.getElementById("task"),t=document.getElementById("deadline"),s=document.getElementById("priority"),a=e.value,d=t.value,i=s.value;if(a&&d&&i){let n={task:a,deadline:d,priority:i};storedTasks.push(n),localStorage.setItem("tasks",JSON.stringify(storedTasks)),createTaskItem(n),e.value="",t.value="",s.value="Tinggi"}});
